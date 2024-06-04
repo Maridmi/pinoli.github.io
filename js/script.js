b97f46101c3161a310f6a1dae4d5e9e58496e86e
Maridmi <yarova0marina@gmail.com>
2024-06-05 00:38:16 +0300

    mobile v

--- js/script.js
+++ js/script.js
@@ -41,24 +41,11 @@ $("a").on('click', function(event) {
 });
 });
 
-
-
-// CAROUSEL
-
-// var $owl = $('.owl-carousel');
-
-// $owl.children().each( function( index ) {
-//   $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
-// });
-
-// $owl.owlCarousel({
-//   center: true,
-//   loop: true,
-//   items: 5,
-// });
-
-// $(document).on('click', '.owl-item>div', function() {
-//   // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
-//   var $speed = 300;  // in ms
-//   $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
-// });
+function navMobile() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
